6caaf53f1e3dd64d0e620b04c07ba35efbfcddbe
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-05-23 14:49:30 +0200

    Cinema

---
+++ src/main/java/ScreeningRoom.java
@@ -0,0 +1,12 @@
+public class ScreeningRoom {
+    private final int seatNumber=2;
+    private final int seatRow=5;
+    private boolean seatState;
+
+
+
+    public ScreeningRoom(boolean seatState) {
+        this.seatState = seatState;
+    }
+}
+
